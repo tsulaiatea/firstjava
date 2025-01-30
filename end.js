52d859ce92a266f1c0d0a100af23153b15098a15
tsulaiatea <teatsulaiaa@gmail.com>
2025-01-31 00:18:23 +0400

    add task_

--- end.js
+++ end.js
@@ -3,67 +3,67 @@
 
 
 
-let fruits = ["apple", "banana", "cherry"];
-fruits. push("orange");
-console.log(fruits);
+// let fruits = ["apple", "banana", "cherry"];
+// fruits. push("orange");
+// console.log(fruits);
 
 // 2. დავალება: ელემენტის ამოღება 
 //  ამოიღეთ ელემენტი მასივის დასაწყისიდან. 
 // წაშალეთ პირველი ელემენტი და აჩვენეთ დარჩენილი მასივი. 
 
-let numbers = [10, 20, 30, 40]; 
-numbers.shift()
-console.log(numbers);
+// let numbers = [10, 20, 30, 40]; 
+// numbers.shift()
+// console.log(numbers);
 
 
 // 3. დაამატეთ ელემენტი კონკრეტულ ინდექსზე. 
 // let colors = ["red", "blue", "green"]; 
 // // ჩასვით "yellow" ინდექსზე 1. 
 
-let colors = ["red", "blue", "green"]
-colors .splice(1, 0, "yellow")
-console.log(colors);
+// let colors = ["red", "blue", "green"]
+// colors .splice(1, 0, "yellow")
+// console.log(colors);
 
 // 4 ორი მასივი გააერთიანეთ ერთ მასივად. 
 // let animals1 = ["cat", "dog"]; 
 // let animals2 = ["lion", "tiger"]; 
 // გააერთიანეთ მასივები და მიიღეთ ახალი მასივი. 
 
-let animals1 = ["cat", "blue", "dog"];
-let animals2 = ["lion", "tiger"];
-let animals = animals1 . concat(animals2);
-console.log(animals);
+// let animals1 = ["cat", "blue", "dog"];
+// let animals2 = ["lion", "tiger"];
+// let animals = animals1 . concat(animals2);
+// console.log(animals);
 
 // 5 დაალაგეთ მასივი ზრდადობის მიხედვით. 
 // let scores = [50, 30, 20, 10, 40]; 
 // დაალაგეთ მასივი. 
 
-let scores = [50, 30, 20, 10, 40];
-scores . sort();
-console.log(scores);
+// let scores = [50, 30, 20, 10, 40];
+// scores . sort();
+// console.log(scores);
 
 // 6 გაერთიანეთ მასივის ყველა ელემენტი ერთ სტრინგში. 
 // let cities = ["Tbilisi", "Batumi", "Kutaisi"]; 
 // გააერთიანეთ ელემენტები, სადაც მათ შორის იქნება " - ". 
 
-let cities = ["Tbilisi", "Batumi", "Kutaisi"]; 
-let result = cities . join(" - ");
-console.log(result);
+// let cities = ["Tbilisi", "Batumi", "Kutaisi"]; 
+// let result = cities . join(" - ");
+// console.log(result);
 
 // 7 შეაiტრიალეთ მასივი. 
 // let letters = ["a", "b", "c", "d"]; 
 
-let letters = ["a", "b", "c", "d"]; 
-letters . reverse();
-console.log(letters);
+// let letters = ["a", "b", "c", "d"]; 
+// letters . reverse();
+// console.log(letters);
 
 // 8 დაადგინეთ მასივის სიგრძე. 
 // let books = ["Math", "Science", "History"]; 
 // გაარკვიეთ მასივის სიგრძე.
 
-let books = ["Math", "Science", "History"];
-let book = books . length;
-console.log(book);
+// let books = ["Math", "Science", "History"];
+// let book = books . length;
+// console.log(book);
 
 
 // task 9
@@ -133,12 +133,84 @@ console.log(book);
 // let box1 =box.trimEnd();
 // console.log(box1);
 
+// 30 იანვარი task_1
+// დავალება 1: სტრიქონის სიგრძე 
+
+// let text = "home";
+// let text1 = text.length;
+// console.log(text1);
+
+// task_2 შეიტანეთ სტრიქონი და გამოიტანეთ ის მხოლოდ დიდი ასოებით. 
+
+// let text = "home";
+// let text1 = text.toLocaleUpperCase();
+// console.log(text1);
+
+// task_3 შეიტანეთ სტრიქონი და გამოიტანეთ ის მხოლოდ პატარა ასოებით. 
+
+// let text = "HOME";
+// let text1 = text.toLocaleLowerCase();
+// console.log(text1);
+
+// task_4 შეიტანეთ სტრიქონი და შეამოწმეთ, რა სიმბოლოა კონკრეტულ ინდექსზე. 
+
+// let text = "home";
+// let text1 = text.at(2);
+// console.log(text1);
+
+// task_5 შეიტანეთ სტრიქონი და ამოჭერით მხოლოდ ნაწილობრივი სტრიქონი (ინდექსებით განსაზღვრული). 
+
+// let text = "home";
+// let text1 = text.substring(1,3);
+// console.log(text1);
 
 
+// task_6 შეიტანეთ სტრიქონი და შეცვალეთ სიტყვა "hello" სიტყვით "hi". 
+// let text = "home";
+// let text1 = text. replace("home", "hi");
+// console.log (text1);
+
+// task_7 შეიტანეთ სტრიქონი, რომელშიც სიტყვები გამოყოფილია მძიმეთი, და გაყავით სტრიქონი შესაბამის ნაწილებად (მიუთითეთ split() მეთოდზე). 
+
+// let text = "home, street, car";
+// let text1 = text. split(",");
+// console.log (text1);
+
+// task_8 შეამოწმეთ, იწყება თუ არა სტრიქონი სიტყვით "Java"
+
+// let text = "home";
+// let text1 = text. startsWith("java");
+// console.log (text1);
+
+// task_9 შეამოწმეთ, მთავრდება თუ არა სტრიქონი სიტყვით "Script". 
+// let text = "home";
+// let text1 = text. endsWith("script");
+// console.log (text1);
+
+// task_10 შეიტანეთ სტრიქონი და გამეორეთ ის 4-ჯერ. 
+// let text = "home";
+// let text1 = text.repeat(4);
+// console.log (text1);
+
+// task_11 შეიტანეთ სტრიქონი, რომელშიც იქნება ცარიელი ადგილები დასაწყისსა და ბოლოში. წაშალეთ ეს ცარიელი ადგილები. 
+
+// let text = "  home  ";
+// let text1 = text.trim();
+// console.log (text1);
 
 
+// let text = "  home  ";
+// let text1 = text.trimStart();
+// console.log (text1);
 
+// let text = "  home  ";
+// let text1 = text.trimEnd();
+// console.log (text1);
 
+// task_12 შეიტანეთ სტრიქონი და შეამოწმეთ, თუ შეიცავს თუ არა ის სიტყვას "JavaScript". 
+// let text = "home";
+// let text1 = text.includes("javascript");
+// console.log (text1);
 
 
 
