76dabcb183be38c658b184c13bbea102c417fc8f
tsulaiatea <teatsulaiaa@gmail.com>
2025-01-30 23:23:30 +0400

    add task10

--- end.js
+++ end.js
@@ -66,10 +66,24 @@ let book = books . length;
 console.log(book);
 
 
-let number = [1, 2, 3, 4, [11, 12, 13, [50, 60], 14]];
-// console.log(number [2]);
-// console.log(number [4] [0]);
-console.log(number [4] [3] [1]);
+// task 9
+// let number = [1, 2, 3, 4, [11, 12, 13, [50, 60], 14]];
+// // console.log(number [2]);
+// // console.log(number [4] [0]);
+// console.log(number [4] [3] [1]);
+
+// task10
+
+let box = "saba";
+let box1 = box.at(-2);
+console.log(box1);
+
+// let box = "saba";
+// let box1 = box. charAt(3);
+// console.log(box1);
+
+
+
 
 
 
